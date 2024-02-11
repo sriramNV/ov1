6b0acd9627b668348f3589b3b557a7b6160f7d70
Sriram N V <sriram.vilvanathan@gmail.com>
2024-02-11 23:04:28 +0530

    initial main.py file

--- code/main.py
+++ code/main.py
@@ -7,10 +7,16 @@ class Game:
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("project-ov1")
 
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-def run(self):
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+            pygame.display.update()
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
